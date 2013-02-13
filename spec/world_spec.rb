96ad5a0c8bbaee62fbd19707156de7342420262c
Nayan Hajratwala <nayan@chikli.com>
2013-02-13 12:38:38 -0500

    small refactoring of test

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -1,27 +1,24 @@
 describe 'the World' do
   let(:api) { double() }
+  let(:world) { World.new(api) }
 
   it 'sends the proper setBlock command for normal blocks' do
     api.should_receive(:send).with("world.setBlock(10,20,30,20)")
-    world = World.new(api)
     world.block(10,20,30,Block::GLASS)
   end
 
   it 'sends the proper setBlock command for blocks with extra data' do
     api.should_receive(:send).with("world.setBlock(5,6,7,35,14)")
-    world = World.new(api)
     world.block(5,6,7,Wool.new(Color::RED))
   end
 
   it 'sends the proper setBlocks command for normal blocks' do
     api.should_receive(:send).with("world.setBlocks(10,20,30,40,50,60,20)")
-    world = World.new(api)
     world.cube(10,20,30,40,50,60,Block::GLASS)
   end
 
   it 'sends the proper setBlocks command for blocks with extra data' do
     api.should_receive(:send).with("world.setBlocks(5,6,7,9,9,9,35,14)")
-    world = World.new(api)
     world.cube(5,6,7,9,9,9,Wool.new(Color::RED))
   end
 end
